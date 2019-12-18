4db303d596b9142b898109c1ae28dd212407763c
Лев Гродзицкий <piyaotoi@gmail.com>
2019-12-19 00:05:50 +0300

    Проверка версии OpenGL в GLUT

--- Teach_OpenGL/Teach_OpenGL/main.cpp
+++ Teach_OpenGL/Teach_OpenGL/main.cpp
@@ -1,5 +1,36 @@
+#include "GL/glew.h"
+#include "GL/glut.h"
 #include <iostream>
-void main() 
+
+void DetectOLGVersion() 
 {
-	std::cout << "hi";
+	int major, minor;
+	glGetIntegerv(GL_MAJOR_VERSION, &major);
+	glGetIntegerv(GL_MINOR_VERSION, &minor);
+	std::cout << "\n\nOpenGL information:"
+		<< "\n " << (const char*)glGetString(GL_RENDERER)
+		<< "\n " << (const char*)glGetString(GL_VENDOR)
+		<< "\n " << (const char*)glGetString(GL_VERSION)
+		<< "\n " << (const char*)glGetString(GL_SHADING_LANGUAGE_VERSION)
+		<< "\n " << major << "." << minor;
+}
+
+void RenderScene() 
+{
+	DetectOLGVersion();
+}
+
+int main(int argc, char** argv) 
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1000, 700);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Texxt");
+	glutDisplayFunc(RenderScene);
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+	glutMainLoop();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+	return 0;
 }
